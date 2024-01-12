97990ff9d38884041ad946e4628d844288190f7f
Kristifor <km@aizen.io>
2024-01-12 18:33:04 +0200

    Resolving an issue with opening the scanner.

--- lib/components/identity_card/identity_card.viewmodel.dart
+++ lib/components/identity_card/identity_card.viewmodel.dart
@@ -14,6 +14,7 @@ class IdentityCardViewModel extends PageViewModel {
       "/start-auth",
       pageContext,
       TransitionData(next: PageTransition.slideForward),
+      bindingData: identity,
     );
   }
 
