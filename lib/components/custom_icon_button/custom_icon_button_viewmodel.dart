1154903caffdf71384747143d48485f2d9b52237
Kristifor <km@aizen.io>
2024-01-27 15:24:09 +0200

    Removing the hard depedency on components

--- lib/components/custom_icon_button/custom_icon_button_viewmodel.dart
+++
@@ -1,5 +0,0 @@
-import 'package:stacked/stacked.dart';
-
-class CustomIconButtonViewModel extends BaseViewModel {
-  ready(bool playAnim) {}
-}
--- lib/components/main_navigation/main_navigation_viewmodel.dart
+++
@@ -1,7 +0,0 @@
-import 'package:presentation/page_view_model.dart';
-
-class MainNavigationViewModel extends PageViewModel {
-  MainNavigationViewModel(super.context);
-
-  onMenuPressed() {}
-}
