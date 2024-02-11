a9792c8ac506a827dcddb65d2be192878775d9d3
Kristifor <km@aizen.io>
2024-02-11 11:38:40 +0200

    adding the business rules to add a pattern

--- lib/views/setup_pattern/setup_pattern_viewmodel.dart
+++ lib/views/setup_pattern/setup_pattern_viewmodel.dart
@@ -1,5 +1,36 @@
+import 'package:domain/models/enums.dart';
+import 'package:infrastructure/interfaces/iauthorization_service.dart';
 import 'package:shared/page_view_model.dart';
 
 class SetupPatternViewModel extends PageViewModel {
-  SetupPatternViewModel(super.context);
+  late IAuthorizationService _authorizationService;
+  bool _isConfirm = false;
+  get isConfirm => _isConfirm;
+  SetupPatternViewModel(super.context) {
+    _authorizationService = getIt.get<IAuthorizationService>();
+  }
+
+  List<int> _filledPattern = [];
+
+  onPatternFilled(List<int> input) async {
+    switch (_filledPattern.isEmpty) {
+      case true:
+        var matching = input == _filledPattern;
+        if (!matching) return;
+
+        var convertToString = "";
+
+        input.forEach((e) {
+          convertToString += "${e},";
+        });
+
+        await _authorizationService.setDeviceLockType(
+          DeviceLockType.pattern,
+          value: convertToString,
+        );
+      case false:
+        _filledPattern = input;
+        break;
+    }
+  }
 }
