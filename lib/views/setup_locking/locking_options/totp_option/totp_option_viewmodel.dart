7b85db7989b795c6a37edce5807fc4600165493d
Kristifor <km@aizen.io>
2024-02-11 14:25:43 +0200

    Adding the business rules to create and set a TOTP lock.

--- lib/views/setup_locking/locking_options/totp_option/totp_option_viewmodel.dart
+++ lib/views/setup_locking/locking_options/totp_option/totp_option_viewmodel.dart
@@ -1,7 +1,15 @@
+import 'package:domain/models/enums.dart';
+import 'package:domain/models/transition_data.dart';
 import 'package:shared/page_view_model.dart';
 
 class TotpOptionViewModel extends PageViewModel {
   TotpOptionViewModel(super.context);
 
-  onToTpEnabled() {}
+  onToTpEnabled() {
+    router.changePage(
+      "/setup-totp-lock",
+      pageContext,
+      TransitionData(next: PageTransition.slideForward),
+    );
+  }
 }
---
+++ lib/views/setup_totp_lock/setup_totp_lock_viewmodel.dart
@@ -0,0 +1,63 @@
+import 'package:domain/exceptions/base_exception.dart';
+import 'package:domain/models/enums.dart';
+import 'package:domain/models/otp_code.dart';
+import 'package:domain/models/transition_data.dart';
+import 'package:infrastructure/interfaces/iauthorization_service.dart';
+import 'package:shared/page_view_model.dart';
+
+class SetupTotpLockViewModel extends PageViewModel {
+  late IAuthorizationService _authorizationService;
+  String _secret = "";
+  SetupTotpLockViewModel(super.context) {
+    _authorizationService = getIt.get<IAuthorizationService>();
+  }
+
+  onScanComplete(String secret) async {
+    if (!secret.contains("otpauth://totp/")) {
+      throw BaseException(context: pageContext, message: "Invalid QR Link");
+    }
+
+    var otpLink = OtpCode.fromQrCodeLink(secret);
+    _secret = otpLink.secret;
+
+    var setLockType = await _authorizationService.setDeviceLockType(
+      DeviceLockType.totp,
+      value: _secret,
+    );
+    if (!setLockType) {
+      throw BaseException(context: pageContext, message: "Invalid QR Link");
+    }
+
+    router.changePage(
+      "/passwords",
+      pageContext,
+      TransitionData(next: PageTransition.easeInAndOut),
+    );
+  }
+
+  onSecretChanged(String secret) async {
+    if (!secret.contains("otpauth://totp/")) {
+      throw BaseException(context: pageContext, message: "Invalid Link");
+    }
+
+    var otpLink = OtpCode.fromQrCodeLink(secret);
+
+    _secret = otpLink.secret;
+
+    var setLockType = await _authorizationService.setDeviceLockType(
+      DeviceLockType.totp,
+      value: _secret,
+    );
+    if (!setLockType) {
+      throw BaseException(context: pageContext, message: "Invalid Link");
+    }
+
+    router.changePage(
+      "/passwords",
+      pageContext,
+      TransitionData(next: PageTransition.easeInAndOut),
+    );
+  }
+
+  onSaveChanges() {}
+}
