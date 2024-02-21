86c04bb4b24ff64ceb3bcd3dea49a6979b712f91
Kristifor <km@aizen.io>
2024-02-21 17:19:19 +0200

    Adding busines rules to issue a challange when the user selects one of the local devices to connect with.

--- lib/views/establish_connection/establish_connection_viewmodel.dart
+++ lib/views/establish_connection/establish_connection_viewmodel.dart
@@ -1,9 +1,15 @@
+import 'package:domain/models/device.dart';
 import 'package:shared/page_view_model.dart';
 
 class EstablishConnectionViewModel extends PageViewModel {
   String _pairCode = "21345";
   String get pairCode => _pairCode;
   EstablishConnectionViewModel(super.context);
+  late Device _device;
+
+  ready() {
+    _device = router.getPageBindingData() as Device;
+  }
 
   onPairPressed() {}
 }
