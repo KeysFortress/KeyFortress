593a44b3fb105642e16fd275f0a7a33f0200592b
Kristifor <km@aizen.io>
2024-02-22 17:41:32 +0200

    Implementing the establish connection business rules.

--- lib/views/establish_connection/establish_connection_viewmodel.dart
+++ lib/views/establish_connection/establish_connection_viewmodel.dart
@@ -4,9 +4,9 @@ import 'package:shared/page_view_model.dart';
 
 class EstablishConnectionViewModel extends PageViewModel {
   late ILocalNetworkService _networkService;
-
   String _pairCode = "21345";
   String get pairCode => _pairCode;
+  String _challange = "";
   EstablishConnectionViewModel(super.context) {
     _networkService = getIt.get<ILocalNetworkService>();
   }
@@ -15,9 +15,10 @@ class EstablishConnectionViewModel extends PageViewModel {
 
   ready() async {
     _device = router.getPageBindingData() as Device;
-    var challanage = await _networkService.issueChallange(_device);
-    // _networkService.connectToDevice(_device.ip, challange, signature)
+    _challange = await _networkService.requestChallange(_device);
   }
 
-  onPairPressed() {}
+  onPairPressed() {
+    _networkService.connectToDevice(_device, _challange);
+  }
 }
