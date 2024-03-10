d67690595db16a69a5362add4d5faefad75d6878
Kristifor Milchev <km@airzen.io>
2024-03-10 16:03:48 +0200

    Removing the hardcoded totp card style and replacing it with the new reusable totp card.

--- lib/views/totp/totp_view.dart
+++ lib/views/totp/totp_view.dart
@@ -1,16 +1,10 @@
-import 'package:components/custom_button/custom_button.dart';
 import 'package:components/dashboard_toggle/dasboard_toggle.dart';
-import 'package:components/horizontal_divider/horizontal_divider.dart';
 import 'package:components/main_navigation/main_navigation.dart';
-import 'package:components/nav_menu_inner/nav_menu_inner.dart';
 import 'package:components/navigation_menu/navigation_menu.dart';
-import 'package:components/seconds_counter/seconds_counter.dart';
 import 'package:domain/models/enums.dart';
 import 'package:domain/styles.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:components/totp_card/totp_card.dart';
 import 'package:presentation/views/totp/totp_viewmodel.dart';
 import 'package:shared/base_component_toggle.dart';
 import 'package:stacked/stacked.dart';
@@ -68,63 +62,8 @@ class TotpView extends StatelessWidget {
               flex: 1,
               child: ListView.builder(
                 itemCount: viewModel.secrets.length,
-                itemBuilder: (context, index) => Container(
-                  padding: EdgeInsets.all(15),
-                  margin: EdgeInsets.fromLTRB(16, 4, 16, 0),
-                  decoration: BoxDecoration(
-                    color: ThemeStyles.theme.background200,
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  child: Stack(
-                    children: [
-                      Column(
-                        children: [
-                          Text(
-                            viewModel.getCode(
-                              viewModel.secrets.elementAt(index),
-                            ),
-                            style: ThemeStyles.regularHeading,
-                          ),
-                          HorizontalDivider(),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                viewModel.secrets.elementAt(index).address,
-                                style: ThemeStyles.regularParagraphOv(
-                                  size: 12,
-                                  color: ThemeStyles.theme.primary300,
-                                ),
-                              ),
-                              Text(
-                                viewModel.secrets.elementAt(index).issuer,
-                                style: ThemeStyles.regularParagraphOv(
-                                  size: 12,
-                                  color: ThemeStyles.theme.primary300,
-                                ),
-                              )
-                            ],
-                          )
-                        ],
-                      ),
-                      Positioned(
-                        left: 10,
-                        child: SecoondsCounter(),
-                      ),
-                      Positioned(
-                        right: 10,
-                        child: CustomButton(
-                          widget: SvgPicture.asset(
-                            'assets/images/bin.svg',
-                            package: 'domain',
-                          ),
-                          callback: () => viewModel.onDeletePressed(
-                            viewModel.secrets.elementAt(index),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
+                itemBuilder: (context, index) => TotpCard(
+                  otpCode: viewModel.secrets.elementAt(index),
                 ),
               ),
             ),
