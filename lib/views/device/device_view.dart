888ed66b9b9372b5cd6ebedf79af08362b761f75
Kristifor Milchev <km@airzen.io>
2024-03-11 17:31:40 +0200

    fixing device view safe area issues.

--- lib/views/device/device_view.dart
+++ lib/views/device/device_view.dart
@@ -16,182 +16,199 @@ class DeviceView extends StatelessWidget {
       viewModelBuilder: () => DeviceViewModel(context),
       onViewModelReady: (viewModel) => viewModel.ready(),
       builder: (context, viewModel, child) => Material(
-        color: ThemeStyles.theme.background300,
+        color: ThemeStyles.theme.primary300,
         child: Column(
           children: [
-            NavMenuInner(
-              location: viewModel.deviceName,
-              callback: () => viewModel.router.backToPrevious(context),
-            ),
-            const SizedBox(height: 8),
-            Container(
-              margin: EdgeInsets.only(left: 4, right: 4),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Expanded(
-                    child: SimpleToggle(
-                      height: 110,
-                      isActive: viewModel.activeType == SyncTypes.full,
-                      icon: "assets/images/sync.svg",
-                      title: "Full Sync",
-                      onPressed: viewModel.fullSyncPreessed,
-                    ),
-                  ),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: SimpleToggle(
-                      height: 110,
-                      isActive: viewModel.activeType == SyncTypes.partial,
-                      icon: "assets/images/checklist.svg",
-                      title: "Partial Sync",
-                      onPressed: viewModel.onPartialSync,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              margin: EdgeInsets.all(4),
-              child: SimpleToggle(
-                height: 110,
-                isActive: viewModel.activeType == SyncTypes.otc,
-                icon: "assets/images/one-time-sync.svg",
-                title: "One time connection",
-                onPressed: viewModel.onOneTimeSelected,
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              margin: EdgeInsets.fromLTRB(4, 4, 4, 0),
-              padding: const EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                color: ThemeStyles.theme.background100,
-                borderRadius: BorderRadius.circular(4),
-              ),
-              child: Text(
-                "Connection History",
-                style: ThemeStyles.regularHeading,
-                textAlign: TextAlign.center,
+            SafeArea(
+              child: NavMenuInner(
+                location: viewModel.deviceName,
+                callback: () => viewModel.router.backToPrevious(context),
               ),
             ),
             Expanded(
-              child: ListView.builder(
-                itemCount: 12,
-                shrinkWrap: true,
-                itemBuilder: (context, index) => Stack(
+              child: Container(
+                color: ThemeStyles.theme.background300,
+                child: Column(
                   children: [
+                    const SizedBox(height: 8),
                     Container(
-                      padding: const EdgeInsets.all(15),
-                      margin: const EdgeInsets.fromLTRB(8, 4, 8, 0),
-                      decoration: BoxDecoration(
-                        color: ThemeStyles.theme.background200,
-                        borderRadius: BorderRadius.circular(4),
-                      ),
-                      child: Column(
+                      margin: EdgeInsets.only(left: 4, right: 4),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          const SizedBox(
-                            height: 32,
-                          ),
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.public,
-                                color: ThemeStyles.theme.primary300,
-                                size: 35,
-                              ),
-                              const SizedBox(width: 8),
-                              Text(
-                                "dawdawd awd d awd ad wad ad ",
-                                style: ThemeStyles.regularParagraphOv(
-                                  color: ThemeStyles.theme.primary300,
-                                ),
-                              )
-                            ],
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                children: [
-                                  Icon(
-                                    Icons.date_range,
-                                    color: ThemeStyles.theme.primary300,
-                                    size: 35,
-                                  ),
-                                  const SizedBox(width: 8),
-                                  Text(
-                                    "10/05/2024",
-                                    style: ThemeStyles.regularParagraphOv(
-                                      color: ThemeStyles.theme.primary300,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Text(
-                                "12:24",
-                                style: ThemeStyles.regularParagraphOv(
-                                  color: ThemeStyles.theme.primary300,
-                                ),
-                              )
-                            ],
-                          ),
-                          CustomButton(
-                            widget: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Icon(
-                                  Icons.arrow_downward,
-                                  color: ThemeStyles.theme.accent200,
-                                  size: 15,
-                                ),
-                                Text(
-                                  "View Details",
-                                  style: ThemeStyles.regularParagraph,
-                                )
-                              ],
+                          Expanded(
+                            child: SimpleToggle(
+                              height: 110,
+                              isActive: viewModel.activeType == SyncTypes.full,
+                              icon: "assets/images/sync.svg",
+                              title: "Full Sync",
+                              onPressed: viewModel.fullSyncPreessed,
                             ),
-                            callback: () {},
-                          )
+                          ),
+                          const SizedBox(width: 8),
+                          Expanded(
+                            child: SimpleToggle(
+                              height: 110,
+                              isActive:
+                                  viewModel.activeType == SyncTypes.partial,
+                              icon: "assets/images/checklist.svg",
+                              title: "Partial Sync",
+                              onPressed: viewModel.onPartialSync,
+                            ),
+                          ),
                         ],
                       ),
                     ),
-                    Positioned(
-                      top: 4,
-                      left: 8,
-                      child: Container(
-                        width: 120,
-                        padding: EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                          color: ThemeStyles.theme.primary300,
-                          borderRadius: BorderRadius.only(
-                            bottomRight: Radius.circular(4),
-                            topLeft: Radius.circular(4),
-                          ),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    Container(
+                      width: double.infinity,
+                      margin: EdgeInsets.all(4),
+                      child: SimpleToggle(
+                        height: 110,
+                        isActive: viewModel.activeType == SyncTypes.otc,
+                        icon: "assets/images/one-time-sync.svg",
+                        title: "One time connection",
+                        onPressed: viewModel.onOneTimeSelected,
+                      ),
+                    ),
+                    Container(
+                      width: double.infinity,
+                      margin: EdgeInsets.fromLTRB(4, 4, 4, 0),
+                      padding: const EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        color: ThemeStyles.theme.background100,
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      child: Text(
+                        "Connection History",
+                        style: ThemeStyles.regularHeading,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: 12,
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) => Stack(
                           children: [
-                            Icon(
-                              Icons.connect_without_contact,
-                              size: 20,
-                              color: ThemeStyles.theme.text300,
+                            Container(
+                              padding: const EdgeInsets.all(15),
+                              margin: const EdgeInsets.fromLTRB(8, 4, 8, 0),
+                              decoration: BoxDecoration(
+                                color: ThemeStyles.theme.background200,
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                              child: Column(
+                                children: [
+                                  const SizedBox(
+                                    height: 32,
+                                  ),
+                                  Row(
+                                    children: [
+                                      Icon(
+                                        Icons.public,
+                                        color: ThemeStyles.theme.primary300,
+                                        size: 35,
+                                      ),
+                                      const SizedBox(width: 8),
+                                      Text(
+                                        "dawdawd awd d awd ad wad ad ",
+                                        style: ThemeStyles.regularParagraphOv(
+                                          color: ThemeStyles.theme.primary300,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Icon(
+                                            Icons.date_range,
+                                            color: ThemeStyles.theme.primary300,
+                                            size: 35,
+                                          ),
+                                          const SizedBox(width: 8),
+                                          Text(
+                                            "10/05/2024",
+                                            style:
+                                                ThemeStyles.regularParagraphOv(
+                                              color:
+                                                  ThemeStyles.theme.primary300,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      Text(
+                                        "12:24",
+                                        style: ThemeStyles.regularParagraphOv(
+                                          color: ThemeStyles.theme.primary300,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                  CustomButton(
+                                    widget: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                          Icons.arrow_downward,
+                                          color: ThemeStyles.theme.accent200,
+                                          size: 15,
+                                        ),
+                                        Text(
+                                          "View Details",
+                                          style: ThemeStyles.regularParagraph,
+                                        )
+                                      ],
+                                    ),
+                                    callback: () {},
+                                  )
+                                ],
+                              ),
                             ),
-                            Text(
-                              "OTC Sync",
-                              style: ThemeStyles.regularParagraphOv(
-                                color: ThemeStyles.theme.text300,
+                            Positioned(
+                              top: 4,
+                              left: 8,
+                              child: Container(
+                                width: 120,
+                                padding: EdgeInsets.all(5),
+                                decoration: BoxDecoration(
+                                  color: ThemeStyles.theme.primary300,
+                                  borderRadius: BorderRadius.only(
+                                    bottomRight: Radius.circular(4),
+                                    topLeft: Radius.circular(4),
+                                  ),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Icon(
+                                      Icons.connect_without_contact,
+                                      size: 20,
+                                      color: ThemeStyles.theme.text300,
+                                    ),
+                                    Text(
+                                      "OTC Sync",
+                                      style: ThemeStyles.regularParagraphOv(
+                                        color: ThemeStyles.theme.text300,
+                                      ),
+                                    )
+                                  ],
+                                ),
                               ),
                             )
                           ],
                         ),
                       ),
-                    )
+                    ),
                   ],
                 ),
               ),
-            ),
+            )
           ],
         ),
       ),
