30fdea37443a8f1de8edd3204ec6e888fb8577a4
Kristifor Milchev <km@airzen.io>
2024-03-11 17:31:27 +0200

    fixing connection list secret safe area issues.

--- lib/views/connection_list/connection_list.dart
+++ lib/views/connection_list/connection_list.dart
@@ -3,6 +3,7 @@ import 'package:components/custom_text_field/custom_text_field.dart';
 import 'package:components/nav_menu_inner/nav_menu_inner.dart';
 import 'package:domain/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:presentation/views/connection_list/connection_list_viewmodel.dart';
 import 'package:stacked/stacked.dart';
@@ -16,121 +17,139 @@ class ConnectionListView extends StatelessWidget {
       viewModelBuilder: () => ConnectionListViewModel(context),
       onViewModelReady: (viewModel) => viewModel.ready(),
       builder: (context, viewModel, child) => Material(
-        color: ThemeStyles.theme.background300,
+        color: ThemeStyles.theme.primary300,
         child: Column(
           children: [
-            NavMenuInner(
-              location: "Local Devices",
-              callback: () => viewModel.router.backToPrevious(context),
-            ),
-            Container(
-              height: 50,
-              margin: EdgeInsets.fromLTRB(20, 4, 20, 0),
-              child: CustomTextField(
-                borderRadius: 5,
-                prefixIcon: Icon(
-                  Icons.search,
-                ),
-                floatingLabel: "Search",
-                onChange: viewModel.onSearchChanged,
+            SafeArea(
+              child: NavMenuInner(
+                location: "Local Devices",
+                callback: () => viewModel.router.backToPrevious(context),
               ),
             ),
             Expanded(
-              flex: 1,
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: viewModel.devices.length,
-                itemBuilder: (context, index) => CustomButton(
-                  callback: () => viewModel.onDeviceSelected(
-                    viewModel.devices.elementAt(index),
-                  ),
-                  widget: Container(
-                    margin: const EdgeInsets.fromLTRB(16, 8, 16, 0),
-                    decoration: BoxDecoration(
-                      color: ThemeStyles.theme.background200,
-                      borderRadius: BorderRadius.circular(4),
+              child: Container(
+                color: ThemeStyles.theme.background300,
+                child: Column(
+                  children: [
+                    Container(
+                      height: 50,
+                      margin: EdgeInsets.fromLTRB(20, 4, 20, 0),
+                      child: CustomTextField(
+                        borderRadius: 5,
+                        prefixIcon: Icon(
+                          Icons.search,
+                        ),
+                        floatingLabel: "Search",
+                        onChange: viewModel.onSearchChanged,
+                      ),
                     ),
-                    child: Column(
-                      children: [
-                        Container(
-                          padding: EdgeInsets.all(5),
-                          decoration: BoxDecoration(
-                            color: ThemeStyles.theme.primary300,
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(4),
-                              topRight: Radius.circular(4),
+                    Expanded(
+                      flex: 1,
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: viewModel.devices.length,
+                        itemBuilder: (context, index) => CustomButton(
+                          callback: () => viewModel.onDeviceSelected(
+                            viewModel.devices.elementAt(index),
+                          ),
+                          widget: Container(
+                            margin: const EdgeInsets.fromLTRB(16, 8, 16, 0),
+                            decoration: BoxDecoration(
+                              color: ThemeStyles.theme.background200,
+                              borderRadius: BorderRadius.circular(4),
+                            ),
+                            child: Column(
+                              children: [
+                                Container(
+                                  padding: EdgeInsets.all(5),
+                                  decoration: BoxDecoration(
+                                    color: ThemeStyles.theme.primary300,
+                                    borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(4),
+                                      topRight: Radius.circular(4),
+                                    ),
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Icon(
+                                            Icons.private_connectivity_rounded,
+                                            size: 20,
+                                            color: ThemeStyles.theme.text300,
+                                          ),
+                                          const SizedBox(width: 4),
+                                          Text(
+                                            viewModel.devices
+                                                .elementAt(index)
+                                                .ip,
+                                            style:
+                                                ThemeStyles.regularParagraphOv(
+                                              color: ThemeStyles.theme.text300,
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                      Row(
+                                        children: [
+                                          Text(
+                                            viewModel.devices
+                                                .elementAt(index)
+                                                .mac,
+                                            style:
+                                                ThemeStyles.regularParagraphOv(
+                                              color: ThemeStyles.theme.text300,
+                                            ),
+                                          ),
+                                          const SizedBox(width: 4),
+                                          Icon(
+                                            Icons.laptop_mac,
+                                            size: 20,
+                                            color: ThemeStyles.theme.text300,
+                                          ),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                Container(
+                                  padding: const EdgeInsets.all(15),
+                                  child: Column(
+                                    children: [
+                                      SvgPicture.asset(
+                                        viewModel.getDeviceType(index),
+                                        package: "domain",
+                                        width: 40,
+                                        height: 80,
+                                        colorFilter: ColorFilter.mode(
+                                          ThemeStyles.theme.primary300,
+                                          BlendMode.srcIn,
+                                        ),
+                                      ),
+                                      Text(
+                                        viewModel.devices.elementAt(index).name,
+                                        style: ThemeStyles.innerHeadingOv(
+                                          color: ThemeStyles.theme.accent200,
+                                          fontSize: 18,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                )
+                              ],
                             ),
                           ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                children: [
-                                  Icon(
-                                    Icons.private_connectivity_rounded,
-                                    size: 20,
-                                    color: ThemeStyles.theme.text300,
-                                  ),
-                                  const SizedBox(width: 4),
-                                  Text(
-                                    viewModel.devices.elementAt(index).ip,
-                                    style: ThemeStyles.regularParagraphOv(
-                                      color: ThemeStyles.theme.text300,
-                                    ),
-                                  )
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Text(
-                                    viewModel.devices.elementAt(index).mac,
-                                    style: ThemeStyles.regularParagraphOv(
-                                      color: ThemeStyles.theme.text300,
-                                    ),
-                                  ),
-                                  const SizedBox(width: 4),
-                                  Icon(
-                                    Icons.laptop_mac,
-                                    size: 20,
-                                    color: ThemeStyles.theme.text300,
-                                  ),
-                                ],
-                              )
-                            ],
-                          ),
                         ),
-                        Container(
-                          padding: const EdgeInsets.all(15),
-                          child: Column(
-                            children: [
-                              SvgPicture.asset(
-                                viewModel.getDeviceType(index),
-                                package: "domain",
-                                width: 40,
-                                height: 80,
-                                colorFilter: ColorFilter.mode(
-                                  ThemeStyles.theme.primary300,
-                                  BlendMode.srcIn,
-                                ),
-                              ),
-                              Text(
-                                viewModel.devices.elementAt(index).name,
-                                style: ThemeStyles.innerHeadingOv(
-                                  color: ThemeStyles.theme.accent200,
-                                  fontSize: 18,
-                                ),
-                              )
-                            ],
-                          ),
-                        )
-                      ],
+                      ),
                     ),
-                  ),
+                    const SizedBox(
+                      height: 16,
+                    )
+                  ],
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 16,
             )
           ],
         ),
