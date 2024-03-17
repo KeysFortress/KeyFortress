d2d0aef8fc57872d491f0803a600a353babca596
Kristifor Milchev <km@airzen.io>
2024-03-17 17:24:55 +0200

    Adding the chart and the connected devices list.

--- lib/views/dashboard/dashboard_view.dart
+++ lib/views/dashboard/dashboard_view.dart
@@ -1,9 +1,17 @@
+import 'package:components/horizontal_divider/horizontal_divider.dart';
 import 'package:components/main_navigation/main_navigation.dart';
+import 'package:components/secret_card/secret_card.dart';
 import 'package:components/simple_toggle/simple_toggle.dart';
+import 'package:domain/models/enums.dart';
+import 'package:domain/models/stored_secret.dart';
 import 'package:domain/styles.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:presentation/views/dashboard/dashboard_viewmodel.dart';
 import 'package:stacked/stacked.dart';
+import 'package:charts_painter/chart.dart';
 
 class DashboardView extends StatelessWidget {
   const DashboardView({super.key});
@@ -17,6 +25,154 @@ class DashboardView extends StatelessWidget {
         child: Column(
           children: [
             SafeArea(child: MainNavigation()),
+            Container(
+              padding: const EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: ThemeStyles.theme.background200,
+                borderRadius: BorderRadius.circular(4),
+              ),
+              child: Column(
+                children: [
+                  Chart(
+                    state: ChartState<void>(
+                      data: ChartData.fromList(
+                        [10, 5, 8, 2, 3, 6]
+                            .map(
+                              (e) => ChartItem<void>(
+                                e.toDouble(),
+                                value: "test",
+                              ),
+                            )
+                            .toList(),
+                      ),
+                      itemOptions: BarItemOptions(
+                        padding: const EdgeInsets.symmetric(horizontal: 2.0),
+                        barItemBuilder: (_) => BarItem(
+                          color: ThemeStyles.theme.primary300,
+                        ),
+                      ),
+                      foregroundDecorations: [],
+                    ),
+                  ),
+                  Divider(
+                    thickness: 1,
+                    color: ThemeStyles.theme.primary300,
+                  ),
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          "10 Passwords",
+                          style: ThemeStyles.regularParagraphOv(
+                            color: ThemeStyles.theme.primary300,
+                            size: 10,
+                          ),
+                        ),
+                        Text(
+                          "5 TOTP",
+                          style: ThemeStyles.regularParagraphOv(
+                            color: ThemeStyles.theme.primary300,
+                            size: 10,
+                          ),
+                        ),
+                        Text(
+                          "8 Identities",
+                          style: ThemeStyles.regularParagraphOv(
+                            color: ThemeStyles.theme.primary300,
+                            size: 10,
+                          ),
+                        ),
+                        Text(
+                          "2 RAC",
+                          style: ThemeStyles.regularParagraphOv(
+                            color: ThemeStyles.theme.primary300,
+                            size: 10,
+                          ),
+                        ),
+                        Text(
+                          "3 RLC",
+                          style: ThemeStyles.regularParagraphOv(
+                            color: ThemeStyles.theme.primary300,
+                            size: 10,
+                          ),
+                        ),
+                        Text(
+                          "3 Secrets",
+                          style: ThemeStyles.regularParagraphOv(
+                            color: ThemeStyles.theme.primary300,
+                            size: 10,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.only(top: 16),
+                padding: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: ThemeStyles.theme.background200,
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                child: viewModel.connectedDevices.isEmpty
+                    ? Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SvgPicture.asset(
+                            "assets/images/computer.svg",
+                            package: 'domain',
+                            fit: BoxFit.contain,
+                            height: 80,
+                            colorFilter: ColorFilter.mode(
+                              ThemeStyles.theme.primary300,
+                              BlendMode.srcIn,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 16,
+                          ),
+                          Text(
+                            "Oops. it appears that there are no nearby devices connected, please check if you're connected over wifi network as the device you're trying to reach.",
+                            style: ThemeStyles.regularParagraphOv(
+                              size: 12,
+                              color: ThemeStyles.theme.primary300,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      )
+                    : Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Connected Devices",
+                            style: ThemeStyles.regularParagraph,
+                          ),
+                          Expanded(
+                            child: ListView.builder(
+                              itemCount: 25,
+                              itemBuilder: (context, index) => SecretCard(
+                                secret: StoredSecret(
+                                  id: "",
+                                  name: "dawdwa",
+                                  username: "",
+                                  content: "",
+                                  lastUsed: DateTime.now(),
+                                  secretType: SecretType.password,
+                                ),
+                                copyEnabled: false,
+                                onActionCallback: () {},
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+              ),
+            ),
             Expanded(
               child: Container(
                 color: ThemeStyles.theme.background300,
@@ -24,7 +180,11 @@ class DashboardView extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     Container(
-                      margin: EdgeInsets.only(left: 8, right: 8),
+                      padding: EdgeInsets.only(left: 8, right: 8, top: 8),
+                      decoration: BoxDecoration(
+                        color: ThemeStyles.theme.background200,
+                        borderRadius: BorderRadius.circular(4),
+                      ),
                       child: Column(
                         children: [
                           Row(
@@ -70,7 +230,7 @@ class DashboardView extends StatelessWidget {
                                 child: SimpleToggle(
                                   height: 120,
                                   width: double.infinity,
-                                  icon: "",
+                                  icon: "assets/images/rac.svg",
                                   title: "RAC",
                                   isActive: false,
                                   onPressed: () {},
@@ -81,7 +241,7 @@ class DashboardView extends StatelessWidget {
                                 child: SimpleToggle(
                                   height: 120,
                                   width: double.infinity,
-                                  icon: "",
+                                  icon: "assets/images/rlc.svg",
                                   title: "RLC",
                                   isActive: false,
                                   onPressed: () {},
@@ -93,7 +253,7 @@ class DashboardView extends StatelessWidget {
                                   height: 120,
                                   width: double.infinity,
                                   icon: "assets/images/sync.svg",
-                                  title: "Connect",
+                                  title: "Pair",
                                   isActive: false,
                                   onPressed: () {},
                                 ),
