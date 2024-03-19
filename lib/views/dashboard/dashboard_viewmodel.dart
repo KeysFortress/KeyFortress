023993148bc11a7e9f9ac96bb8491754950d970e
Kristifor Milchev <km@airzen.io>
2024-03-19 23:27:59 +0200

    Adding the business rules to load the chart data.

--- lib/views/dashboard/dashboard_viewmodel.dart
+++ lib/views/dashboard/dashboard_viewmodel.dart
@@ -1,9 +1,46 @@
-import 'package:domain/models/device.dart';
+import 'package:infrastructure/interfaces/iidentity_manager.dart';
+import 'package:infrastructure/interfaces/iotp_service.dart';
+import 'package:infrastructure/interfaces/isecret_manager.dart';
 import 'package:shared/page_view_model.dart';
 
 class DashboardViewModel extends PageViewModel {
-  DashboardViewModel(super.context);
+  late IIdentityManager _identityManager;
+  late ISecretManager _secretManager;
+  late IOtpService _otpService;
 
-  List<Device> _connectedDevices = [];
-  List<Device> get connectedDevices => _connectedDevices;
+  int _identities = 0;
+  int get identities => _identities;
+
+  int _secrets = 0;
+  int get secrets => _secrets;
+
+  int _otpSecrets = 0;
+  int get optSecrets => _otpSecrets;
+
+  int _personalSecrets = 0;
+  int get personalSecrets => _personalSecrets;
+
+  int _rlc = 0;
+  int get rlc => _rlc;
+
+  int _rac = 0;
+  int get rac => _rac;
+
+  DashboardViewModel(super.context) {
+    _identityManager = getIt.get<IIdentityManager>();
+    _secretManager = getIt.get<ISecretManager>();
+    _otpService = getIt.get<IOtpService>();
+  }
+
+  ready() async {
+    var identities = await _identityManager.getSecrets();
+    var secrets = await _secretManager.getSecrets();
+    var otpSecrets = await _otpService.get();
+
+    _identities = identities.length;
+    _secrets = secrets.length;
+    _otpSecrets = otpSecrets.length;
+
+    notifyListeners();
+  }
 }
