bb9ca73a6fbc182cacbed2e14bad4e35c3617643
Kristifor Milchev <km@airzen.io>
2024-03-24 16:09:18 +0200

    Adding a route for the auto fill setting view.

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -5,6 +5,7 @@ import 'package:go_router/go_router.dart';
 import 'package:infrastructure/interfaces/ipage_router_service.dart';
 import 'package:presentation/views/add_otp_code/add_otp_code_view.dart';
 import 'package:presentation/views/add_otp_secret/add_otp_secret.dart';
+import 'package:presentation/views/auto_fill_setting/auto_fill_setting.dart';
 import 'package:presentation/views/pair_settings/pair_settings.dart';
 import 'package:presentation/views/connect_device_screen/connect_device_screen_view.dart';
 import 'package:presentation/views/connection_list/connection_list.dart';
@@ -231,6 +232,12 @@ class ApplicationRouter {
     (
       "pair-settings",
       PairSettings(),
+      1,
+      Duration(milliseconds: 500),
+    ),
+    (
+      "auto-fill-settings",
+      Autofill(),
       1,
       Duration(milliseconds: 500),
     )
